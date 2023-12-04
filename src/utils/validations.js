7e3dcdeae1c994f045c35a9a65b47670ccb1b759
Rania khalifa <rania.khalifa96@gmail.com>
2023-12-04 16:05:29 +0100

    fix validation for the inputs

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,8 +1,9 @@
 import * as Yup from "yup";
 
+const regexForLocation = /^[0-9-.]+$/
 export const locationValidationSchema = Yup.object({
-    min_lon: Yup.string().required("Minimim Longitude is required"),
-    min_lat: Yup.string().required("Minimim Latitude is required"),
-    max_lon: Yup.string().required("Maximum Longitude is required"),
-    max_lat: Yup.string().required("Maximum Latitude is required"),
+    min_lon: Yup.string().required("Minimim Longitude is required").matches(regexForLocation, "Must be only digits"),
+    min_lat: Yup.string().required("Minimim Latitude is required").matches(regexForLocation, "Must be only digits"),
+    max_lon: Yup.string().required("Maximum Longitude is required").matches(regexForLocation, "Must be only digits"),
+    max_lat: Yup.string().required("Maximum Latitude is required").matches(regexForLocation, "Must be only digits"),
 });
